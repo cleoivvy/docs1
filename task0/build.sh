87c1254062764734a0798064efca4212e1ed8e2a
cleoivvy <claraojobo@gmail.com>
2025-01-30 22:50:42 +0100

    fix:deployment stage

--- task0/build.sh
+++ task0/build.sh
@@ -2,10 +2,10 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r $(dirname "$0")/requirements.txt
+pip install -r requirements.txt
 
 # Convert static asset files
-python $(dirname "$0")/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python $(dirname "$0")/manage.py migrate
+python manage.py migrate
