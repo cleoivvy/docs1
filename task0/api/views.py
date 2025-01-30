384ae459266af60baf09817423db8aeb3ec990b2
cleoivvy <claraojobo@gmail.com>
2025-01-30 23:16:48 +0100

    fix: still changes

--- task0/api/views.py
+++ task0/api/views.py
@@ -3,13 +3,16 @@ from django.http import JsonResponse
 from datetime import datetime
 
 def Get_info(request):
-    email = "claraojobo@gmail.com"
-    current_datetime = datetime.utcnow().isoformat() + 'Z'  
-    github_url = "https://github.com/cleoivvy/docs1.git"
-  
-    data = {
-        'email': email,
-        'current_datetime': current_datetime,
-        'github_url': github_url
-    }
-    return JsonResponse(data, status=200)
+    try:
+        email = "claraojobo@gmail.com"
+        current_datetime = datetime.utcnow().isoformat() + 'Z'  
+        github_url = "https://github.com/cleoivvy/docs1.git"
+      
+        data = {
+            'email': email,
+            'current_datetime': current_datetime,
+            'github_url': github_url
+        }
+        return JsonResponse(data, status=200)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
